eadf6feea0f73d46d6f9e83c54d3fa6b92562dba
Vimal7686 <vimal76869@gmail.com>
2020-10-16 14:33:01 +0530

    Ready To Deploy

---
+++ backend/build/precache-manifest.87e9fdee9dbc76ac8f1c60d8474f1856.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c81529bf82430baca089eb035751055",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36e5502c550784a8f0b7",
+    "url": "/static/css/main.706448ca.chunk.css"
+  },
+  {
+    "revision": "b639c79332b3cc29d371",
+    "url": "/static/js/2.e0b289b8.chunk.js"
+  },
+  {
+    "revision": "851820dc40bca3bcaf80cf6dd26069f9",
+    "url": "/static/js/2.e0b289b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e5502c550784a8f0b7",
+    "url": "/static/js/main.16065677.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
